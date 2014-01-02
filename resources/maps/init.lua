6d797b5df7311324aeeeee41783f56623b78084a
Etienne <etienne@gauvin.info>
2014-01-03 00:11:14 +0100

    Rétablissement du chargement automatique des maps

--- resources/maps/init.lua
+++ resources/maps/init.lua
@@ -1,44 +1,7 @@
 -- Chargement des maps
 
-return {
-  forest = {
-    [1] = core.atl.Loader.load('/forest/1.tmx')
-  }
-}
-
---[[
--- Dossier des maps
-local mapsPath = core.atl.Loader.path
-local path = '.'
-
--- Fonction récursive de chargement
-local function load(path)
-
-  -- Tableau
-  local res = {}
-  
-  -- Liste des dossiers
-  local files = love.filesystem.getDirectoryItems(mapsPath .. '/' .. path)
-  print(mapsPath .. '/' .. path, #files)  
-  
-  -- Charger tout le dossier
-  for f, fileName in ipairs(files) do
-    local filePath = string.gsub(mapsPath .. '/' .. path .. '/' .. fileName, '%./', '')
-    local fileRelativePath = string.gsub(path .. '/' .. fileName, '%./', '')
-    local name = string.match(fileName, '^[^.]+')
-    
-    -- Fichier
-    if love.filesystem.isFile(filePath) and fileName ~= 'init.lua' then
-      local map = core.atl.Loader.load('/' .. fileRelativePath)
-      res[tonumber(name) or name] = map
-    
-    -- Dossier
-    elseif love.filesystem.isDirectory(filePath) then
-      res[tonumber(fileName) or fileName] = load(fileRelativePath)
-    end
-  end
-  
-  return res
+local function load(filePath)
+  return core.atl.Loader.load(filePath)
 end
 
-return load(path)]]
+return core.path.load('resources/maps', '%.tmx$', load)
