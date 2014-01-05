f91433ff1593ea01dfefd9ddc6542ff5c9e10e7b
Etienne <etienne@gauvin.info>
2014-01-05 23:06:22 +0100

    Mise à jour du tileset

--- resources/tilesets/forest.tsx
+++ resources/tilesets/forest.tsx
@@ -1,29 +1,27 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="forest" tilewidth="48" tileheight="48">
- <image source="forest.png" width="384" height="384"/>
+ <image source="forest.png" width="336" height="192"/>
  <terraintypes>
-  <terrain name="grassyGround" tile="11"/>
-  <terrain name="earthGround" tile="51"/>
+  <terrain name="grassy-ground" tile="-1"/>
  </terraintypes>
- <tile id="6" terrain="0,0,0,"/>
- <tile id="7" terrain="0,0,,0"/>
- <tile id="10" terrain=",,,0"/>
- <tile id="11" terrain=",,0,0"/>
- <tile id="12" terrain=",,0,"/>
- <tile id="14" terrain="0,,0,0"/>
- <tile id="15" terrain=",0,0,0"/>
- <tile id="18" terrain=",0,,0"/>
- <tile id="19" terrain="0,0,0,0"/>
- <tile id="20" terrain="0,,0,"/>
- <tile id="22" terrain="0,0,0,0"/>
- <tile id="23" terrain="0,0,0,0"/>
- <tile id="26" terrain=",0,,0"/>
- <tile id="27" terrain="0,0,0,0"/>
- <tile id="28" terrain="0,,0,"/>
- <tile id="34" terrain=",0,,"/>
- <tile id="35" terrain="0,0,,"/>
- <tile id="36" terrain="0,,,"/>
- <tile id="50" terrain=",,,1"/>
- <tile id="51" terrain=",,1,1"/>
- <tile id="52" terrain=",,1,"/>
+ <tile id="1" terrain=",,,0"/>
+ <tile id="2" terrain=",,0,0"/>
+ <tile id="3" terrain=",,0,0"/>
+ <tile id="4" terrain=",,0,"/>
+ <tile id="5" terrain="0,0,0,"/>
+ <tile id="6" terrain="0,0,,0"/>
+ <tile id="8" terrain=",0,,0"/>
+ <tile id="9" terrain="0,0,0,0"/>
+ <tile id="10" terrain="0,0,0,0"/>
+ <tile id="11" terrain="0,,0,"/>
+ <tile id="12" terrain="0,,0,0"/>
+ <tile id="13" terrain=",0,0,0"/>
+ <tile id="15" terrain=",0,,0"/>
+ <tile id="16" terrain="0,0,0,0"/>
+ <tile id="17" terrain="0,0,0,0"/>
+ <tile id="18" terrain="0,,0,"/>
+ <tile id="22" terrain=",0,,"/>
+ <tile id="23" terrain="0,0,,"/>
+ <tile id="24" terrain="0,0,,"/>
+ <tile id="25" terrain="0,,,"/>
 </tileset>
