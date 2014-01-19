9a91c3b8871c4ee14bfb0d499abeb61e36d565a5
Etienne <etienne@gauvin.info>
2014-01-19 02:14:40 +0100

    Ajout d'un tileset de ronces

---
+++ resources/tilesets/dangerous.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="dangerous" tilewidth="48" tileheight="48">
+ <image source="dangerous.png" width="144" height="48"/>
+ <terraintypes>
+  <terrain name="brambles" tile="-1"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,"/>
+</tileset>
