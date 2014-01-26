2f8084dd898fbe94f2ecbdc0b1b18d2c52489b4b
Etienne <etiennegauvin41@gmail.com>
2014-01-26 15:32:33 +0100

    Déplacement du blocage de la caméra dans une autre classe

--- scripts/states/play.lua
+++ scripts/states/play.lua
@@ -19,6 +19,9 @@ end
 function PlayState:start(Level)
   -- Démarrage du niveau
   self.level = Level:new()
+  
+  -- Personnage
+  --self.level:getCharacter('main-character')
 end
 
 -- Arrêt
@@ -29,34 +32,6 @@ end
 function PlayState:update(dt)
   self.level:update(dt)
   
-  if core.debug.freeCamera then
-    local focus = Vector(
-      self.level.map.width * self.level.map.tileWidth * mouse.getX() / screen.w(),
-      self.level.map.height * self.level.map.tileHeight * mouse.getY() / screen.h())
-    
-    -- Limite à gauche
-    if focus.x - screen.w() / 2 < 0 then
-      focus.x = screen.w() / 2
-    end
-    
-    -- Limite à droite
-    if focus.x + screen.w() / 2 > self.level.map.width * self.level.map.tileWidth then
-      focus.x = self.level.map.width * self.level.map.tileWidth - screen.w() / 2
-    end
-    
-    -- Limite à gauche
-    if focus.y - screen.h() / 2 < 0 then
-      focus.y = screen.h() / 2
-    end
-    
-    -- Limite en bas
-    if focus.y + screen.h() / 2 > self.level.map.height * self.level.map.tileHeight then
-      focus.y = self.level.map.height * self.level.map.tileHeight - screen.h() / 2
-    end
-    
-    game.camera:lookAt(focus.x, focus.y)
-    self.level.map:setDrawRange(focus.x - screen.w() / 2, focus.y - screen.h() / 2, screen.w(), screen.h())
-  end
 end
 
 -- Affichage
