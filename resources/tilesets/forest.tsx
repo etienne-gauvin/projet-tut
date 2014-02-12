9acbe73e799af00c3ac54fdcb2eb671569bcdc0f
JulianTeilleux <julian.teilleux@gmail.com>
2014-02-12 16:01:45 +0100

    Herbes bord gauche+droite, amélioration de la map 1

--- resources/tilesets/forest.tsx
+++ resources/tilesets/forest.tsx
@@ -1,8 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="forest" tilewidth="48" tileheight="48">
- <image source="forest.png" width="336" height="192"/>
+ <image source="forest.png" width="336" height="384"/>
  <terraintypes>
   <terrain name="grassy-ground" tile="-1"/>
+  <terrain name="dirt-ground" tile="-1"/>
  </terraintypes>
  <tile id="1" terrain=",,,0"/>
  <tile id="2" terrain=",,0,0"/>
@@ -24,4 +25,24 @@
  <tile id="23" terrain="0,0,,"/>
  <tile id="24" terrain="0,0,,"/>
  <tile id="25" terrain="0,,,"/>
+ <tile id="29" terrain=",,,1"/>
+ <tile id="30" terrain=",,1,1"/>
+ <tile id="31" terrain=",,1,1"/>
+ <tile id="32" terrain=",,1,"/>
+ <tile id="33" terrain="1,1,1,"/>
+ <tile id="34" terrain="1,1,,1"/>
+ <tile id="36" terrain=",1,,1"/>
+ <tile id="37" terrain="1,1,1,1"/>
+ <tile id="38" terrain="1,1,1,1"/>
+ <tile id="39" terrain="1,,1,"/>
+ <tile id="40" terrain="1,,1,1"/>
+ <tile id="41" terrain=",1,1,1"/>
+ <tile id="43" terrain=",1,,1"/>
+ <tile id="44" terrain="1,1,1,1"/>
+ <tile id="45" terrain="1,1,1,1"/>
+ <tile id="46" terrain="1,,1,"/>
+ <tile id="50" terrain=",1,,"/>
+ <tile id="51" terrain="1,1,,"/>
+ <tile id="52" terrain="1,1,,"/>
+ <tile id="53" terrain="1,,,"/>
 </tileset>
