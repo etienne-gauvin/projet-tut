e60e874fcf42f41ec38e3ece1c15cbf5c9b48c4f
JulianTeilleux <julian.teilleux@gmail.com>
2014-02-18 15:44:02 +0100

    changement de vitesse

--- scripts/anims/main-character/walk.lua
+++ scripts/anims/main-character/walk.lua
@@ -1,8 +1,8 @@
 local image = resources.images.entities.mainCharacter.walkSpriteSheet
 local grid = core.anim8.newGrid(48, 96, image:getWidth(), image:getHeight())
 
-local animationLeft = core.anim8.newAnimation(grid('6-1',1), 0.5)
-local animationRight = core.anim8.newAnimation(grid('1-6',2), 0.5)
+local animationLeft = core.anim8.newAnimation(grid('6-1',1), 0.15)
+local animationRight = core.anim8.newAnimation(grid('1-6',2), 0.15)
 
 return {
   left = animationLeft,
